028bd07ffb8674e9a93f684bb6d3677f672d7c61
vinay <vinay@VINAY23>
2023-08-27 18:04:53 +0530

    Add project files.

---
+++ WpfApp2/HorizontalTab.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp2
+{
+    public class HorizontalTab : TabControl
+    {
+        public HorizontalTab()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(HorizontalTab), new FrameworkPropertyMetadata(typeof(HorizontalTab)));
+            this.Loaded += HorizontalTab_Loaded;            
+        }
+
+        /// <summary>
+        /// It stores the scrollviewer of tabpanel which I made static property to access it from MainWindow.xaml.cs
+        /// </summary>
+        public static ScrollViewer? _Scroll { get; set; }
+
+        /// <summary>
+        /// This method is used to find the scrollviewer in the control template as we can't acess it directly.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HorizontalTab_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScrollViewer? tabScroll = this.Template.FindName("tabscroll", this) as ScrollViewer;    //"tabscroll" is the name of the scrollviewer.
+            if (tabScroll != null && tabScroll.ComputedHorizontalScrollBarVisibility == Visibility.Visible)
+            {
+                _Scroll = tabScroll;
+                tabScroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Hidden;
+            }
+        }
+
+        /// <summary>
+        /// This dependency property is used as borderbrush.
+        /// </summary>
+        public System.Windows.Media.Brush FrameColor
+        {
+            get { return (System.Windows.Media.Brush)GetValue(FrameColorProperty); }
+            set { SetValue(FrameColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty FrameColorProperty = DependencyProperty.Register(
+            "FrameColor",
+            typeof(System.Windows.Media.Brush),
+            typeof(HorizontalTab),
+            new FrameworkPropertyMetadata(System.Windows.Media.Brushes.Orange, FrameworkPropertyMetadataOptions.AffectsRender));       
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AreTabsFillingWidth();
+        }
+
+        /// <summary>
+        /// This method is used to check if the tab items filled in the tab control or not.
+        /// </summary>
+        private void AreTabsFillingWidth()
+        {   
+            //if the tabs are filled the arrow buttons will visisble.
+            if((tab1.Items.Count * 100) > tab1.Width)
+            {
+                LeftButton.Visibility = Visibility.Visible; 
+                RightButton.Visibility = Visibility.Visible;
+            }       
+        }
+
+        /// <summary>
+        /// This is the event for left button which scolls the scrollviewer to leftside by 3 lines.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbtn_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                HorizontalTab._Scroll.LineLeft();
+            }
+        }
+
+        /// <summary>
+        /// This is the event for right button which scolls the scrollviewer to rightside by 3 lines.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rbtn_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                HorizontalTab._Scroll.LineRight();
+            }
+        }
+    }
+}
